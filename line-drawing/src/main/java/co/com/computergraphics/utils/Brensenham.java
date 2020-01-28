bbbb01a1bc0f6c489d0b4216a2f06986c3824a2c
angrajales <agrajal7@eafit.edu.co>
2020-01-27 21:31:46 -0500

    Add reference

--- line-drawing/src/main/java/co/com/computergraphics/utils/Brensenham.java
+++ line-drawing/src/main/java/co/com/computergraphics/utils/Brensenham.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Brensenham {
+    // This algorithm was taken from: https://es.wikipedia.org/wiki/Algoritmo_de_Bresenham
     public List<Point> getPoints(Point p1, Point p2) {
         int x1 = p1.getX(), y1 = p1.getY(), x2 = p2.getX(), y2 = p2.getY();
         List<Point> points = new ArrayList<>();
