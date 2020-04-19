49406ef37e7d20cfad9ebc92c693f01d5cd07e3a
angrajales <agrajal7@eafit.edu.co>
2020-04-19 11:07:06 -0500

    Ray Tracer

---
+++ RayTracerSinEspecular/src/Scene/Scene.java
@@ -0,0 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Scene;
+
+import java.util.ArrayList;
+import Math.Intersectable;
+import Math.Ray;
+import Math.Solutions;
+
+/**
+ *
+ * @author htrefftz
+ */
+public class Scene {
+    // Ambient light, comes from "everywhere"
+    static AmbientLight ambientLight;
+    // Several Point Lights
+    static ArrayList<PointLight> pointLights = new ArrayList<>();
+    // Several Spheres
+    static ArrayList<Intersectable> intersectables = new ArrayList<>();
+    // Bacground color
+    //static final Colour BACKGROUNDCOLOR = new Colour(0.7, 0.7, 0.9);
+    static final Colour BACKGROUNDCOLOR = new Colour(0.2, 0.2, 0.2);
+    //static final Colour BACKGROUNDCOLOR = new Colour(.1, 0.74 , 1);
+    
+    /**
+     * Set the ambient light in the scene
+     * @param myAmbientLight Ambient light to set
+     */
+    public static void setAmbientLight(AmbientLight myAmbientLight) {
+        ambientLight = myAmbientLight;
+    }
+    
+    /**
+     * Add a point light
+     * @param myPointLight Point Light to be added 
+     */
+    public static void addPointLight(PointLight myPointLight) {
+        pointLights.add(myPointLight);
+    }
+    
+    /**
+     * Add a Sphere
+     * @param mySphere Sphere to be added
+     */
+    public static void addIntersectable(Intersectable i) {
+        intersectables.add(i);
+    }
+
+    /**
+     * Compute the color of the closest object in the Scene this ray 
+     * intersects with
+     * @param ray Ray to intersect with the Scene
+     * @return 
+     */
+    public static Colour intersectRay(Ray ray) {
+        double minT = Double.MAX_VALUE;
+        Intersectable closest = null;
+        // Chose the closest surface
+        for(Intersectable intersectable: intersectables) {
+            Solutions s = intersectable.intersect(ray);
+            if(s.getNumSolutions() > 0) {
+                // ignore solutins with a t value smaller than 0,
+                // because they are behind the origin of the ray
+                if(s.getT1() > 0.01) {
+                    // Is this surface closer to the origin of the ray?
+                    if(s.getT1() < minT) {
+                        minT = s.getT1();
+                        closest = intersectable;
+                    }
+                }
+            }
+        }
+        if(closest != null) {
+            Colour acum  = new Colour(0, 0, 0);
+            double Ko = closest.getMaterial().Ko;
+            double Kr = closest.getMaterial().Kr;
+            double Kt = closest.getMaterial().Kt;
+            // Compute this oject's color
+            if(Ko != 0) {
+                Colour thisColor = Colour.multiply(closest.callShader(ray, minT),Ko);
+                acum = Colour.add(acum, thisColor);
+            }
+            // Compute the reflection
+            // removed calculation or reflection
+            return acum;
+        }
+        return BACKGROUNDCOLOR;
+    }
+    
+    /**
+     * Finds if there is an intersection of any object in the scene with
+     * the given ray.
+     * This is used to compute shadows
+     * @param ray Ray to intersect all objects in the scene with
+     * @return 
+     */
+    public static boolean intersectRayForShadow(Ray ray) {
+        for(Intersectable intersectable: intersectables) {
+            Solutions s = intersectable.intersect(ray);
+            if(s.getNumSolutions() > 0 && s.getT1() > 0.01) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ RayTracerSinEspecular/src/View/Main.java
@@ -0,0 +1,147 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package View;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Color;
+
+import Math.Point;
+import Math.Sphere;
+
+import Scene.Scene;
+import Scene.Colour;
+import Scene.AmbientLight;
+import Scene.PointLight;
+import Scene.Material;
+
+/**
+ *
+ * @author htrefftz
+ */
+public class Main extends JPanel {
+    Image image;
+    private final boolean DEBUG = false;
+    
+    static int width;
+    static int height;
+    
+    int l1x = 100;
+    int l1y = 100;
+    int l1z = 100;
+    
+    public void setImage(Image image) {
+        this.image = image;
+    }
+    
+    /**
+     * Create all elements in the scene
+     * Could be read from a text file
+     */
+    public void createScene() {
+        AmbientLight al = new AmbientLight(new Colour(.2, .2, .2));
+        Scene.setAmbientLight(al);
+        
+        PointLight pl1 = new PointLight(new Point(l1x, l1y, l1z), new Colour(1, 1, 1));
+        //Scene.addPointLight(pl1);
+        Scene.addPointLight(pl1);
+        
+        
+        // A yellow reflective sphere
+        /*
+        double Ka = .2;        // ambient
+        double Kd = .8;        // difuse
+        double Ks = .7;          // specular
+        int n = 32;
+        */
+        double Ka1 = .2;        // ambient
+        double Kd1 = .8;        // difuse
+        double Ks1 = .7;          // specular
+        int n1 = 32;
+        
+        Colour color = new Colour(1, 0, 0);     // object's color
+        double Ko = 1;          // Weight of this object's color
+        double Kr = 0;          // Weight of the reflected color
+        double Kt = 0;          // Weight of the refracted color
+        //Material material1 = new Material(Ka, Kd, Ks, n, color, Ko, Kr, Kt, texture1);
+        Material material1 = new Material(Ka1, Kd1, Ks1, n1, color, Ko, Kr, Kt);
+        
+        Sphere sp1 = new Sphere(new Point(-20, 0, -100), 20, material1);
+        Scene.addIntersectable(sp1);
+        
+        double Ka2 = .2;        // ambient
+        double Kd2 = .8;        // difuse
+        double Ks2 = .7;          // specular
+        int n2 = 64;
+        Colour color2 = new Colour(0, 0, 1);
+        Material material2 = new Material(Ka2, Kd2, Ks2, n2, color2, Ko, Kr, Kt);
+        
+        Sphere sp2 = new Sphere(new Point(20, 0, -100), 20, material2);
+        Scene.addIntersectable(sp2);
+
+    }
+    
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        
+        Graphics2D g2d = (Graphics2D) g;
+
+        Dimension size = getSize();
+
+        int w = width;
+        int h = height;      
+        
+        image = new Image(w, h);
+        image.generateImage();      // Compute the colors of the scene
+        this.setImage(image);
+
+        
+        Colour colour;
+        for(int i = 0; i < image.height; i++) {
+            for(int j = 0; j < image.width; j++) {
+                colour = image.image[i][j];
+                int red = (int) (colour.getR() * 255);
+                int green = (int) (colour.getG() * 255);
+                int blue = (int) (colour.getB() * 255);
+                // Clamp out of range colors
+                if(red > 255) red = 255;
+                if(green > 255) green = 255;
+                if(blue > 255) blue = 255;
+                g2d.setColor(new Color(red, green, blue));
+                if(DEBUG)
+                    System.out.println(red + " " + green + " " + blue);
+                g2d.drawLine(i, j, i, j);
+            }
+        }        
+    }
+    
+    
+    public static void main(String [] args) {
+        JFrame frame = new JFrame("Ray Tracer 2020");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        width = 500;
+        height = 500;
+        
+        // Create this JPanel
+        Main main = new Main();
+        main.createScene();         // Add lights, objects, etc.
+        // Texture image = new Texture(width, height);
+        // image.generateImage();      // Compute the colors of the scene
+        // main.setImage(image);
+
+        // Draw the result
+        frame.add(main);
+        frame.setSize(width, height);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        
+    }
+}
